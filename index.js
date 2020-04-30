c4ae714e4cea2aff68098450b052c61026e41193
Arif Swozon <swazan.arif@gmail.com>
2020-04-30 10:59:18 +0600

    auth using passport

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000, //30 days
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!');
-});
+authRoutes(app);
 
 app.listen(PORT);
