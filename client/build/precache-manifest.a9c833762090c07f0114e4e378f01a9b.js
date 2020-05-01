70ead5f1951b117d364de551bdfe9ea225ba8bce
Arif Swozon <swazan.arif@gmail.com>
2020-05-01 15:35:55 +0600

    prod build

---
+++ client/build/precache-manifest.a9c833762090c07f0114e4e378f01a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145f79523c0f897a9c05b5b232dda0eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17e3b49a3687b9def0f3",
+    "url": "/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "17e3b49a3687b9def0f3",
+    "url": "/static/js/2.a5d2ca43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a5d2ca43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b3363f654dc45d9c45",
+    "url": "/static/js/main.7c2bb345.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
